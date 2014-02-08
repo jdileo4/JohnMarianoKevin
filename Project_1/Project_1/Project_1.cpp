727049c7694f9a096fe8854951acdb5c49918be9
msalem07 <msalem@tamu.edu>
2014-02-07 18:56:25 -0600

    Added the Project with the files and Unit test

---
+++ Project_1/Project_1/Project_1.cpp
@@ -0,0 +1,6 @@
+// Project_1.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ Project_1/UnitTest1/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "stdafx.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(DEFINITION)
+		{
+			
+		}
+
+	};
+}
