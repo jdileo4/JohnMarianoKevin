8abcda6f50f6b1f72a01233b8aa66ed59434f238
kevinhan66 <rtkevinhan66@gmail.com>
2014-02-10 04:48:07 -0600

    Filled in helper functions
    I wasn't sure what to do with the unit test files so I just left them
    alone. I simply added the helper functions (addColumns(), addRow(),
    etc). I'm a bit lost on what we have accomplished so far (not to be rude
    or anything). If someone could fill the group in on exactly which
    functions we have completed and not would be nice. Sorry for not working
    on it as much this weekend; Did the best I could with the helper
    functions

---
+++ kevin_update files/Table.h
@@ -0,0 +1,27 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "Column.h"
+
+using namespace std;
+
+class Table{
+private:
+	int id;
+	string name;
+
+	vector<string> keys;
+	vector<Column> columns;
+
+public:
+	Table(string tableName, vector<string> tableKeys)
+		: name(tableName), keys(tableKeys){}
+
+	string getName() { return name; }
+	int getID() { return id; }
+
+	void addRow(vector<Datum> rowData);
+	void addColumn(string name, string type);
+};
+
+#endif
